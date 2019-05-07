35068d46b9e3a4ebae76b5d05162f83101a4dc85
Tyson Morris <temorris87@yahoo.com>
2019-05-07 09:32:41 -0400

    Game initialized with a default hard coded board.

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -1,9 +1,20 @@
 let boardWidth = 9;
 let divBoard = document.querySelector('#board');
 
+let defaultBoard = [['4', ' ', '8', ' ', ' ', '7', '2', ' ', '1'],
+                    [' ', '3', ' ', ' ', '8', ' ', '9', ' ', ' '],
+                    ['6', '7', '5', ' ', '1', ' ', ' ', '3', ' '],
+                    [' ', '5', ' ', ' ', ' ', '4', ' ', '2', '3'],
+                    ['3', ' ', ' ', '5', ' ', ' ', '1', '6', ' '],
+                    [' ', ' ', ' ', ' ', ' ', ' ', '7', ' ', '5'],
+                    [' ', ' ', '3', '9', ' ', ' ', ' ', ' ', '7'],
+                    [' ', ' ', '1', '7', '6', '3', ' ', '4', ' '],
+                    ['5', ' ', ' ', ' ', '4', '2', '3', ' ', '9']];
+
 class Cell {
     constructor(id, element) {
         this.id = id;
+        this.number = 0;
         this.element = element;
         this.notes = [false, false, false, false, false, false, false, false, false];
     }
@@ -12,6 +23,7 @@ class Cell {
 class Board {
     constructor() {
         this.createCells();
+        this.initBoard(defaultBoard);
     }
 
     addDividingLines() {
@@ -32,6 +44,19 @@ class Board {
         }
     }
 
+    initBoard(board) {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                this.getCell(i, j).number = board[i][j];
+            }
+        }
+        this.redrawEntireBoard();
+    }
+
+    getCell(i, j) {
+        return this[`cell-${i}${j}`];
+    }
+
     createCell(i, j) {
         let id = 'cell-' + i.toString() + j.toString();
         let element = document.createElement('div');
@@ -50,6 +75,19 @@ class Board {
             }
         }
         this.addDividingLines();
+    }
+
+    redrawEntireBoard() {
+        let currentCell;
+        for (let i = 0; i < boardWidth; i++) {
+            for (let j = 0; j < boardWidth; j++) {
+                currentCell = this.getCell(i, j);
+                if (currentCell.number !== 0) {
+                    console.log('hello');
+                    currentCell.element.innerText = currentCell.number.toString();
+                }
+            }
+        }
     }
 }
 
