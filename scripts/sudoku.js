7395319df4d5b49d296c36655f180f7fa697431e
Tyson Morris <temorris87@yahoo.com>
2019-05-07 12:59:37 -0400

    Refactored code to be better modularized with Cell. Finished hover overlay.

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -12,11 +12,29 @@ let defaultBoard = [['4', ' ', '8', ' ', ' ', '7', '2', ' ', '1'],
                     ['5', ' ', ' ', ' ', '4', '2', '3', ' ', '9']];
 
 class Cell {
-    constructor(id, element) {
-        this.id = id;
+    constructor(i, j) {
+        this.i = i;
+        this.j = j;
+        this.id = this.getCellId(i, j);
         this.number = 0;
-        this.element = element;
+        this.element = document.createElement('div');
+
+        this.element.id = this.id;
+        this.element.classList.add('cell');
+
         this.notes = [false, false, false, false, false, false, false, false, false];
+    }
+
+    getCellId() {
+        return `cell-${this.i}${this.j}`;
+    }
+
+    static getCellId(i, j) {
+        return `cell-${i}${j}`;
+    }
+
+    static getCellIndices(cellId) {
+        return { i: cellId[5], j: cellId[6] };
     }
 }
 
@@ -26,6 +44,7 @@ class Board {
         this.targetCell = null;
         this.createCells();
         this.initBoard(defaultBoard);
+        this.startEventListeners();
     }
 
     addDividingLines() {
@@ -54,8 +73,9 @@ class Board {
         let currentCell;
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
-                currentCell = this.getCell(i, j);
+                currentCell = this[Cell.getCellId(i, j)];
                 currentCell.number = board[i][j];
+                divBoard.appendChild(currentCell.element);
 
                 if (board[i][j] === ' ') {
                     currentCell.element.classList.add('default');
@@ -65,29 +85,15 @@ class Board {
                 }
             }
         }
-        this.setTargetCell(this.getCell(0, 0));
         this.redrawEntireBoard();
     }
 
-    getCell(i, j) {
-        return this[`cell-${i}${j}`];
-    }
-
-    createCell(i, j) {
-        let id = 'cell-' + i.toString() + j.toString();
-        let element = document.createElement('div');
-
-        element.id = id;
-        element.classList.add('cell');
-        divBoard.appendChild(element);
-
-        this[id] = new Cell(id, element);
-    }
-
     createCells() {
+        let cell;
         for (let i = 0; i < 9; i++) {
             for (let j = 0; j < 9; j++) {
-                this.createCell(i, j);
+                cell = new Cell(i, j);
+                this[cell.id] = cell;
             }
         }
         this.addDividingLines();
@@ -97,7 +103,7 @@ class Board {
         let currentCell;
         for (let i = 0; i < boardWidth; i++) {
             for (let j = 0; j < boardWidth; j++) {
-                currentCell = this.getCell(i, j);
+                currentCell = this[Cell.getCellId(i, j)];
                 if (currentCell.number !== 0) {
                     currentCell.element.innerText = currentCell.number.toString();
                 }
@@ -105,13 +111,37 @@ class Board {
         }
     }
 
+    moveTargetCell(direction) {
+
+    }
+
     setTargetCell(cell) {
-        if (this.targetCell) {
-            this.targetCell.element.classList.remove('target');
+        if (cell.classList.contains('cell')) {
+            if (this.targetCell) {
+                this.targetCell.element.classList.remove('target');
+            }
+            this.targetCell = this[cell.id];
+            this.targetCell.element.classList.add('target');
         }
-        this.targetCell = cell;
-        cell.element.classList.add('target');
-        this.redrawEntireBoard();
+    }
+
+    startEventListeners() {
+        let body = document.querySelector('body');
+
+        // Add key events for moving the target square around.
+        body.addEventListener('mouseover', (event) => {
+            if (event.target.classList.contains('cell')) {
+                this.targetCell = this[event.target.id];
+                this.targetCell.element.classList.add('target');
+            }
+        });
+
+        body.addEventListener('mouseout', (event) => {
+            if (event.target.classList.contains('cell')) {
+                event.target.classList.remove('target');
+                this.targetCell = null;
+            }
+        });
     }
 }
 
